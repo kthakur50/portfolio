6510d3643b23f586d0d0ad96933091578ce43a1d
Kaushal Thakur <kthakur0578@gmail.com>
2026-08-06 14:47:57 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => (
             </div>
 
             <div className="hero-btns">
-              <a href="https://github.com/kaushalt18" target="_blank" rel="noopener" className="btn-o">
+              <a href="https://github.com/kaushalt18" target="_blank" rel="noopener" className="btn-o btn-o--gh">
                 <span className="btn-o-ico">
                   <svg width="13" height="13" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
@@ -74,7 +74,7 @@ const Hero = () => (
                 </span>
                 GitHub
               </a>
-              <a href="https://linkedin.com/in/kaushalt18" target="_blank" rel="noopener" className="btn-o">
+              <a href="https://linkedin.com/in/kaushalt18" target="_blank" rel="noopener" className="btn-o btn-o--li">
                 <span className="btn-o-ico">
                   <svg width="14" height="14" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <rect width="24" height="24" rx="4" fill="#0A66C2"/>
@@ -83,7 +83,7 @@ const Hero = () => (
                 </span>
                 LinkedIn
               </a>
-              <a href="#" className="btn-o">
+              <a href="#" className="btn-o btn-o--rs">
                 <span className="btn-o-ico">
                   <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
