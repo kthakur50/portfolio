2a715a4c9c5d56aa8bae019b002858fd10307dc7
Kaushal Thakur <kthakur0578@gmail.com>
2026-09-04 21:35:07 +0530

    update

--- src/components/SiteBackground.jsx
+++ src/components/SiteBackground.jsx
@@ -8,21 +8,21 @@ const SiteBackground = () => (
   <div className="site-waves-bg" aria-hidden="true">
     <GradientWaves
       horizonColor="#0b0620"
-      waveColor="#4c1d95"
+      waveColor="#6d28d9"
       crestColor="#c4b5fd"
       speed={0.28}
-      amplitude={3.2}
+      amplitude={4.2}
       waveScale={0.55}
       waveRatio={0.9}
-      swell={32}
-      turbulence={16}
+      swell={38}
+      turbulence={20}
       tilt={1.15}
       zoom={0.95}
       height={5.5}
       fogDepth={24}
       detail="medium"
-      brightness={0.6}
-      opacity={0.28}
+      brightness={0.95}
+      opacity={0.6}
       mouseInteraction={true}
       parallaxStrength={0.4}
       grain={true}
